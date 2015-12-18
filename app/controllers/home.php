ca4a86ebb99660290df2790256d994e863692ab6
Samuel ROZE <samuel.roze@gmail.com>
2015-12-18 17:07:25 +0100

    Create the form on the page to register for the newsletter

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,10 +1,40 @@
 <?php
 
-$routes = $app['controllers_factory'];
-$routes->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', [
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
+$routes = $app['controllers_factory'];
+$routes->match('/', function (Request $request) use ($app) {
+    /** @var FormFactoryInterface $formFactory */
+    $formFactory = $app['form.factory'];
+
+    /** @var FormInterface $form */
+    $form = $formFactory->createBuilder(FormType::class)
+        ->add('email', TextType::class, [
+            'constraints' => [
+                new NotBlank(),
+                new Email(),
+            ]
+        ])
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $email = $form->getData()['email'];
+
+        $message = 'Thank you very much, see you soon!';
+    }
+
+    return $app['twig']->render('index.html.twig', [
+        'form' => $form->createView(),
+        'message' => isset($message) ? $message : null,
     ]);
-});
+})->method('GET|POST');
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -1,9 +1,19 @@
 <?php
 
+use Silex\Provider\FormServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ValidatorServiceProvider;
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app = new Silex\Application([
+    'debug' => true,
+]);
+$app->register(new FormServiceProvider());
+$app->register(new TranslationServiceProvider());
+$app->register(new ValidatorServiceProvider());
+$app->register(new TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../app/views',
 ));
 
