0969efbb089d792c5cee370bba97ae9478b48eae
Samuel ROZE <samuel.roze@gmail.com>
2016-03-06 21:22:06 +0000

    Update the contents and reactivate the form

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -19,16 +19,35 @@ $routes->match('/', function (Request $request) use ($app) {
     $form = $formFactory->createBuilder(FormType::class)
         ->add('email', TextType::class, [
             'constraints' => [
-                new NotBlank(),
-                new Email(),
-            ]
+                new NotBlank([
+                    'message' => 'The email address is required',
+                ]),
+                new Email([
+                    'message' => 'The email address should be valid'
+                ]),
+            ],
+            'attr' => [
+                'placeholder' => 'Your email address',
+            ],
+        ])
+        ->add('github', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'The GitHub username is required',
+                ]),
+            ],
+            'attr' => [
+                'placeholder' => 'Your GitHub username',
+            ],
         ])
         ->getForm();
 
     $form->handleRequest($request);
 
     if ($form->isValid()) {
-        $email = $form->getData()['email'];
+        $formData = $form->getData();
+
+        $email = $formData['email'];
         $client = new MailChimpClient('b8453108d758fb6feef42cdb90195868-us12');
 
         try {
@@ -39,6 +58,9 @@ $routes->match('/', function (Request $request) use ($app) {
                 ],
                 'double_optin' => false,
                 'send_welcome' => false,
+                'merge_vars' => [
+                    'github' => $formData['github']
+                ]
             ]);
 
             $message = 'Thank you very much, see you soon!';
