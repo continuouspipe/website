b90aaf1a4706bdc43782f1a42cc787f78a62da32
Samuel ROZE <samuel.roze@gmail.com>
2016-09-23 16:18:57 +0200

    Create the entire documentation structure

--- app/controllers/documentation.php
+++ app/controllers/documentation.php
@@ -2,11 +2,19 @@
 
 $routesFactory = $app['controllers_factory'];
 $routesFactory->match('/', function () use ($app) {
-    return $app['twig']->render('documentation/basics/index.html.twig');
+    return $app['twig']->render('documentation/basics/introduction.html.twig');
 })->method('GET')->bind('documentation');
 
-$routesFactory->match('/configure/', function () use ($app) {
-    return $app['twig']->render('documentation/configure/index.html.twig');
+$routesFactory->match('/basics/{page}/', function ($page) use ($app) {
+    return $app['twig']->render('documentation/basics/'.$page.'.html.twig');
+})->method('GET')->bind('documentation_basics');
+
+$routesFactory->match('/configure/{page}/', function ($page) use ($app) {
+    return $app['twig']->render('documentation/configure/' . $page . '.html.twig');
 })->method('GET')->bind('documentation_configure');
+
+$routesFactory->match('/examples/{page}/', function ($page) use ($app) {
+    return $app['twig']->render('documentation/examples/' . $page . '.html.twig');
+})->method('GET')->bind('documentation_examples');
 
 return $routesFactory;
