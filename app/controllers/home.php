5a6414e4865f61300d4c0c038d6592323cc9becb
Samuel ROZE <samuel.roze@gmail.com>
2016-09-27 17:42:19 +0100

    Rework on the front-end pages

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,6 +1,8 @@
 <?php
 
+use GuzzleHttp\Exception\ClientException;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormFactoryInterface;
@@ -8,19 +10,29 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use ZfrMailChimp\Client\MailChimpClient;
+use Intercom\IntercomClient;
 
 $routes = $app['controllers_factory'];
-$routes->match('/', function (Request $request) use ($app) {
+$routes->match('/contact', function (Request $request) use ($app) {
     /** @var FormFactoryInterface $formFactory */
     $formFactory = $app['form.factory'];
 
     /** @var FormInterface $form */
     $form = $formFactory->createBuilder(FormType::class)
+        ->add('name', TextType::class, [
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'What\'s your name?',
+                ]),
+            ],
+            'attr' => [
+                'placeholder' => 'Your name',
+            ],
+        ])
         ->add('email', TextType::class, [
             'constraints' => [
                 new NotBlank([
-                    'message' => 'The email address is required',
+                    'message' => 'If you don\'t give us an email address we won\'t be able to answer your message!',
                 ]),
                 new Email([
                     'message' => 'The email address should be valid'
@@ -31,13 +43,19 @@ $routes->match('/', function (Request $request) use ($app) {
             ],
         ])
         ->add('github', TextType::class, [
+            'required' => false,
+            'attr' => [
+                'placeholder' => 'Your GitHub username, if any.',
+            ],
+        ])
+        ->add('message', TextareaType::class, [
+            'attr' => [
+                'placeholder' => 'Your message',
+            ],
             'constraints' => [
                 new NotBlank([
-                    'message' => 'The GitHub username is required',
+                    'message' => 'I\'m not sure if we\'ll really be able to help you if you don\'t say anything to us!',
                 ]),
-            ],
-            'attr' => [
-                'placeholder' => 'Your GitHub username',
             ],
         ])
         ->getForm();
@@ -47,34 +65,58 @@ $routes->match('/', function (Request $request) use ($app) {
     if ($form->isValid()) {
         $formData = $form->getData();
 
-        $email = $formData['email'];
-        $client = new MailChimpClient('b8453108d758fb6feef42cdb90195868-us12');
+        $client = new IntercomClient('i0yqsxbt', '425c0c2f4a693ec048e1496cd4169b59071d6ae9');
 
         try {
-            $client->subscribe([
-                'id' => '713f4777b9',
-                'email' => [
-                    'email' => $email
-                ],
-                'double_optin' => false,
-                'send_welcome' => false,
-                'merge_vars' => [
-                    'github' => $formData['github']
-                ]
+            $response = $client->leads->getLeads([
+                'email' => $formData['email'],
             ]);
 
-            $message = 'Thank you very much, see you soon!';
-        } catch (\ZfrMailChimp\Exception\Ls\AlreadySubscribedException $e) {
-            $message = 'Yeah, it looks like you\'ve already subscribed. Thank you again :)';
-        } catch (\ZfrMailChimp\Exception\ExceptionInterface $e) {
+            $leadsWithThisEmail = $response->contacts;
+
+            if (count($leadsWithThisEmail) > 0) {
+                $lead = current($leadsWithThisEmail);
+            } else {
+                $lead = $client->leads->create([
+                    'email' => $formData['email'],
+                    'name' => $formData['name'],
+                    'custom_attributes' => [
+                        'github_username' => (string) $formData['github'],
+                    ]
+                ]);
+            }
+
+            $client->messages->create([
+                'from' => [
+                    'type' => 'contact',
+                    'id' => $lead->id,
+                ],
+                'body' => $formData['message'],
+            ]);
+
+            $message = 'Thank you very much, we will get in touch soon!';
+        } catch (ClientException $e) {
+            echo $e->getResponse()->getBody()->getContents();
             $form->addError(new FormError($e->getMessage()));
         }
     }
 
-    return $app['twig']->render('index.html.twig', [
+    return $app['twig']->render('contact.html.twig', [
         'form' => $form->createView(),
         'message' => isset($message) ? $message : null,
     ]);
-})->method('GET|POST');
+})->method('GET|POST')->bind('contact');
+
+$routes->get('/pricing', function() use ($app) {
+    return $app['twig']->render('pricing.html.twig');
+})->bind('pricing');
+
+$routes->get('/frequently-asked-questions', function() use ($app) {
+    return $app['twig']->render('faq.html.twig');
+})->bind('faq');
+
+$routes->get('/', function() use ($app) {
+    return $app['twig']->render('index.html.twig');
+})->bind('homepage');
 
 return $routes;
