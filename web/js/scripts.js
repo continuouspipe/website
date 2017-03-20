91d7a3c65255446214086d5bf8702da78514d2c2
Samuel ROZE <samuel.roze@gmail.com>
2017-03-20 13:42:05 +0000

    Ensure the hash is changed after smooth scrolling

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -96,12 +96,17 @@
 			$('a[href*=#]:not([href=#])').click(function() {
 				if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-					var target = $(this.hash);
+					var hash = this.hash,
+						target = $(hash);
+
 					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 					if (target.length) {
 						$('html,body').animate({
 							scrollTop: target.offset().top
-						}, 500);
+						}, 500, function (){
+				            location.hash = hash;
+				        });
+
 						return false;
 					}
 				}
