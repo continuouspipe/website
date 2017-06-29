073451e9d13d18b052394be6332b0c3f585c5104
Mark Weston <mweston@inviqa.com>
2017-06-29 13:47:47 +0100

    CD-517: Setup gulp sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 const gulp = require('gulp'),
-      sass = require('gulp-sass');
+      sass = require('gulp-sass'),
+      sourcemaps = require('gulp-sourcemaps');
 
 
 gulp.task('sass', () => {
     gulp.src('./web/sass/**/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('./web/css'));
 });
 
